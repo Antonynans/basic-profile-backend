889392a19f9038a1b9400530b89f78076e7bc351
Antonynans <antonynans@gmail.com>
2021-04-20 21:00:59 +0100

    removed cors

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const port = process.env.PORT || 5000;
 //     return callback(null, true);
 //   }
 // }));
+app.use(cors())
+
 app.use(express.json());
 
 const User = require("./models/userSchema");
@@ -105,9 +107,9 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.get('/', function (req, res) {
-  res.render('index', {});
-});
+// app.get('/', function (req, res) {
+//   res.render('index', {});
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
